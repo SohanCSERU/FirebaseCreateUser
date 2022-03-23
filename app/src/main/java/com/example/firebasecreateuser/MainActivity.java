bd413ebde439c27e8308e2ad5b898acf6b7ee915
SohanCSERU <israkcse@gmail.com>
2022-03-23 16:59:36 +0600

    Color Edit and the data successfully push to the server

--- app/src/main/java/com/example/firebasecreateuser/MainActivity.java
+++ app/src/main/java/com/example/firebasecreateuser/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.firebasecreateuser;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -26,9 +34,56 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//
+
         registerbtn = (TextView) findViewById(R.id.idTVNewUser);
-//        register.setOnClickListener(this);
+        loginBtn = (Button)findViewById(R.id.idBtnLogin);
+
+        userNameEdt = findViewById(R.id.idEdtUserName);
+        passwordEdt = findViewById(R.id.idEdtPassword);
+
+        loadingPB = (ProgressBar) findViewById(R.id.idPBLoading);
+
+
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = userNameEdt.getText().toString().trim();
+                String password = passwordEdt.getText().toString().trim();
+
+                if(name.isEmpty()){
+                    userNameEdt.setError("Email is required!");
+                    userNameEdt.requestFocus();
+                    return;
+                }
+                if (!Patterns.EMAIL_ADDRESS.matcher(name).matches()){
+                    userNameEdt.setError("Please Enter a valid email !");
+                    userNameEdt.requestFocus();
+                    return;
+                }
+                if(password.isEmpty()){
+                    passwordEdt.setError("Password is Required!");
+                    passwordEdt.requestFocus();
+                    return;
+                }
+
+                loadingPB.setVisibility(view.VISIBLE);
+
+                mAuth.createUserWithEmailAndPassword(name,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            //redirecte to user profile
+                            startActivity(new Intent(MainActivity.this,ProfileActivity.class));
+                        }
+                        else{
+                            Toast.makeText(MainActivity.this,"Failed to login!",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+            }
+        });
 
         registerbtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,15 +93,7 @@ public class MainActivity extends AppCompatActivity{
                 startActivity(i);
             }
         });
+
     }
 
-//
-//    @Override
-//    public void onClick(View view) {
-//        switch(view.getId()){
-//            case R.id.idBtnRegister:
-//                startActivity(new Intent(this,RegistrationActivity.class));
-//                break;
-//        }
-//    }
 }
--- app/src/main/java/com/example/firebasecreateuser/RegistrationActivity.java
+++ app/src/main/java/com/example/firebasecreateuser/RegistrationActivity.java
@@ -88,8 +88,7 @@ public class RegistrationActivity extends AppCompatActivity {
                                         Toast.makeText(RegistrationActivity.this,"User Successfully Regestrated",Toast.LENGTH_LONG).show();
 
                                         //Login Redirect
-                                        Intent i = new Intent(RegistrationActivity.this, MainActivity.class);
-                                        startActivity(i);
+                                        startActivity(new Intent(RegistrationActivity.this,LoginActivity.class));
                                     }
                                     else{
                                         Toast.makeText(RegistrationActivity.this,"Failed To  Regestrated",Toast.LENGTH_LONG).show();
