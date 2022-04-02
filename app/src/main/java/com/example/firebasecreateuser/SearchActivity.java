51987c1ece9886dfb650f0af4bd9d49155ddc195
SohanCSERU <israkcse@gmail.com>
2022-04-02 14:27:37 +0600

    Added Some undo Commit

---
+++ app/src/main/java/com/example/firebasecreateuser/SearchActivity.java
@@ -0,0 +1,99 @@
+package com.example.firebasecreateuser;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.firebasecreateuser.databinding.ActivitySearchBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class SearchActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+
+    private GoogleMap mMap;
+    private ActivitySearchBinding binding;
+    ArrayList<LatLng> arrayList = new ArrayList<LatLng>();
+    ArrayList<String> list = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivitySearchBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("User");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                arrayList.clear();
+                for (DataSnapshot snap: snapshot.getChildren()){
+                    User user =  snap.getValue(User.class);
+                    String text = user.latitude;
+                    String text1 = user.longitude;
+
+                    list.add(user.blood_group);
+
+                    Double lat = Double.parseDouble(user.latitude);
+                    Double lon = Double.parseDouble(user.longitude);
+
+                    mMap.addMarker(new MarkerOptions().position(new LatLng(lat, lon)).title(user.blood_group)).showInfoWindow();
+                    mMap.animateCamera(CameraUpdateFactory.zoomTo(15f));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(lat,lon)));
+
+
+                }
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onMarkerClick(@NonNull Marker marker) {
+
+        Intent i = new Intent(SearchActivity.this,MainActivity.class);
+        startActivity(i);
+
+        return false;
+    }
+}
